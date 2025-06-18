3a838076303aa089029a5ee83df2c35e79e046e4
nicolas-proz <NICK93@uba.ar>
2025-06-18 07:04:44 -0300

    empiezo p7

--- Practicas/p8/14.pl
+++ Practicas/p8/14.pl
@@ -1,2 +1,25 @@
 :- consult('9.pl').
+cuadradoSemiMagico(N, M) :-
+    desdeReversible(0,SumaFila),
+    matriz_con_suma(N, N, SumaFila, M).
+
+matriz_con_suma(0, _, _, []).
+matriz_con_suma(NF, NC, SumaFila, [Fila|Resto]) :-
+    NF > 0,
+    fila_con_suma(NC, SumaFila, Fila),
+    NF1 is NF - 1,
+    matriz_con_suma(NF1, NC, SumaFila, Resto).
+
+fila_con_suma(0, 0, []).
+fila_con_suma(Len, Suma, [X|Xs]) :-
+    Len > 0,
+    between(0, Suma, X),
+    Suma1 is Suma - X,
+    Len1 is Len - 1,
+    fila_con_suma(Len1, Suma1, Xs).
+
+
+
+    
+    
 
